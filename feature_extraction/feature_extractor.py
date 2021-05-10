66214247c3313aa31c33603b82682d5c6974ccd4
fbab920 <fbab920@aucklanduni.ac.nz>
2021-05-11 11:32:38 +1200

    Initial commit

---
+++ feature_extraction/feature_extractor.py
@@ -0,0 +1,12 @@
+class FeatureExtractor:
+    def __init__(self, text):
+        self.text = text
+    '''
+    TO DO
+    def extract_text_features(self):
+        # this is temporary command to get features.
+        # It needs to be replaced with proper feature extractor for Eng-NZ
+        # Right now the features are related to US English
+        # One possible solution: http://robotspeech.auckland.ac.nz:59125/process?INPUT_TEXT=%T%&INPUT_TYPE=TEXT&OUTPUT_TYPE=TARGETFEATURES&LOCALE=en_NZ
+
+    '''
