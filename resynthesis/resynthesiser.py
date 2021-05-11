f81ba2aab6c85c74291ec53954514e814db80efb
fbab920 <fbab920@aucklanduni.ac.nz>
2021-05-11 14:40:30 +1200

    Initial commit

---
+++ resynthesis/resynthesiser.py
@@ -0,0 +1,12 @@
+class Resynthesiser:
+    def __init__(self, pac):
+        self.pac = pac
+    
+    '''
+    # TO DO
+    def resynthesis():
+    
+    def __pac_to_pitchtier():
+
+    def __generate_wav():
+    '''
