5d3bdaa6b840518b7a9a7b7df8f59f34955862c1
fbab920 <fbab920@aucklanduni.ac.nz>
2021-05-11 21:53:10 +1200

    Implement creator

--- tts_creator.py
+++ tts_creator.py
@@ -2,6 +2,7 @@ from sound_creation.sound_creator import SoundCreator
 from fujisaki_extractor.fujisaki_extractor import FujisakiExtractor
 from feature_extraction.feature_extractor import FeatureExtractor
 from resynthesis.resynthesiser import Resynthesiser
+import os
 
 class TTSCreator:
     def __init__(self, text, emotion):
@@ -10,23 +11,34 @@ class TTSCreator:
         self.sound_creator = SoundCreator(text)
         self.fujisaki_extractor = FujisakiExtractor(text)
         self.feature_extractor = FeatureExtractor(text)
-        self.resynthesiser = Resynthesiser()
+        self.resynthesiser = Resynthesiser(emotion)
 
 
     def create_speech(self):
 
         sound_file = self.sound_creator.create_sound()
-        '''
-        # TO DO
         fujisaki_features = self.fujisaki_extractor.extract_features(sound_file)
-        text_features = self.feature_extractor.extract_features(self.text)
-        speech_with_emotion = self.resynthesiser.resynthesise()
-        '''
+        text_features = self.feature_extractor.extract_text_features()
+        merged_csv = self.__merge_textfeat_and_fujifeat(fujisaki_features, text_features)
+        pitchtier = self.resynthesiser.resynthesis(merged_csv, self.fujisaki_extractor.pac, self.fujisaki_extractor.lab)
 
+        self.__generate_wav_from_pitchtier(sound_file, pitchtier)
+        
         return "input: " + self.text + " " + self.emotion
 
     def __merge_textfeat_and_fujifeat(self, fuji, textfeat):
         import pandas as pd
+
         dfs = [pd.read_csv(fuji), pd.read_csv(textfeat)]
-        df_merged = pd.concat(dfs, ignore_index=True, axis=1)
+        df_merged = pd.concat(dfs, axis=1)
         df_merged.to_csv('text_and_fujifeat.csv')
+
+        return os.path.realpath('text_and_fujifeat.csv')
+
+    def __generate_wav_from_pitchtier(self, wave, pitchtier):
+        cmd = r'bin\Praat.exe --run bin\wav_and_pitchtier_to_wav.psc ' + wave + ' ' + pitchtier
+        os.system(cmd)
+
+if __name__ == '__main__':
+    tts = TTSCreator('Water harms the new born boy', 'sad')
+    tts.create_speech()
