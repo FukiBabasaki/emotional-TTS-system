da8789bfae262095096d1ecd93c65ddd60f3bfb1
fbab920 <fbab920@aucklanduni.ac.nz>
2021-05-11 18:05:16 +1200

    implement merge_csv

--- tts_creator.py
+++ tts_creator.py
@@ -1,4 +1,8 @@
-from sound-creation.sound_creator import SoundCreator
+from sound_creation.sound_creator import SoundCreator
+from fujisaki_extractor.fujisaki_extractor import FujisakiExtractor
+from feature_extraction.feature_extractor import FeatureExtractor
+from resynthesis.resynthesiser import Resynthesiser
+
 class TTSCreator:
     def __init__(self, text, emotion):
         self.text = text
@@ -14,9 +18,15 @@ class TTSCreator:
         sound_file = self.sound_creator.create_sound()
         '''
         # TO DO
-        fujisaki_features = self.fujisaki_extractor.extract_features()
-        text_features = self.feature_extractor.extract_features()
+        fujisaki_features = self.fujisaki_extractor.extract_features(sound_file)
+        text_features = self.feature_extractor.extract_features(self.text)
         speech_with_emotion = self.resynthesiser.resynthesise()
         '''
 
         return "input: " + self.text + " " + self.emotion
+
+    def __merge_textfeat_and_fujifeat(self, fuji, textfeat):
+        import pandas as pd
+        dfs = [pd.read_csv(fuji), pd.read_csv(textfeat)]
+        df_merged = pd.concat(dfs, ignore_index=True, axis=1)
+        df_merged.to_csv('text_and_fujifeat.csv')
